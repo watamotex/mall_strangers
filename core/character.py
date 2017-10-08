3dd8eaed6e4cb8cb28853c149f5b88ad4c70d451
watamotex <umaru@openmailbox.org>
2017-10-08 19:41:04 +0300

    Character moving animation

--- core/character.py
+++ core/character.py
@@ -10,6 +10,8 @@ class Character:
     """
 
     def __init__(self):
+        self.pos = {'x': 0, 'y': 0}
+        self.SPEED = 2
         self.clothes = {
             'cap': None,
             'shirt': None,
@@ -18,9 +20,32 @@ class Character:
         self.inventory = []
         # here is money
         self.wallet = 0
+        self.current_animation = "idle"
 
-    def get_sprite(self):
+    def get_sprite(self, time):
         rect = (0, 0, 16, 23)
-        return self.spritesheet.return_animation("idle")
+        return self.spritesheet.return_animation(self.current_animation, time)
+
+    def draw(self, screen, time):
+        screen.blit(self.get_sprite(time), dest=(self.pos['x'], self.pos['y']))
+
+    def move(self, destination):
+        if destination == 'down':
+            self.current_animation = "move_down"
+            self.pos['y'] += self.SPEED
+        elif destination == 'up':
+            self.current_animation = "move_up"
+            self.pos['y'] -= self.SPEED
+        elif destination == 'left':
+            self.current_animation = "move_left"
+            self.pos['x'] -= self.SPEED
+        elif destination == 'right':
+            self.current_animation = "move_right"
+            self.pos['x'] += self.SPEED
+        else:
+            self.current_animation = "idle"
+
+    def idle(self):
+        self.current_animation = "idle"
 
 
